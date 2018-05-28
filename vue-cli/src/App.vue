4f2da51b5e2e0d3e00e2f2226aca097f1de81302
stevchow <stephen_chow97@yahoo.co.id>
2018-05-28 21:16:35 +0700

    app.vue is the main page to put all of our .vue components, .vue file is a single component with template,script,and style.. remember to export and import when you want to use component

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,21 +1,10 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-  </div>
+  <nationality-flag></nationality-flag>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+
 </script>
 
 <style>
-
 </style>
---
+++ vue-cli/src/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{flag}} {{nationality}}</h1>
+    <button @click="changeNationality">Change Nationality</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      flag : "🇮🇩",
+      nationality: "INA"
+    };
+  },
+  methods: {
+    changeNationality() {
+      this.flag = "🌏"
+      this.nationality = "WORLD";
+    }
+  }
+};
+</script>
