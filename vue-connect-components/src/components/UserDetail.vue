8f0f271fe287a67f1e5b5e9dc1f287b74298e941
stevchow <stephen_chow97@yahoo.co.id>
2018-05-30 22:17:06 +0700

    passing props from outside to child with v-bind

--- vue-connect-components/src/components/UserDetail.vue
+++ vue-connect-components/src/components/UserDetail.vue
@@ -2,7 +2,7 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>{{name}}</p>
+        <p>My name is {{name}}</p>
     </div>
 </template>
 
