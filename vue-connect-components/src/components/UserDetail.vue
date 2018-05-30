78858c44a84d94fc87960a84405dac0e9f76b2ac
stevchow <stephen_chow97@yahoo.co.id>
2018-05-30 22:20:24 +0700

    naming props

--- vue-connect-components/src/components/UserDetail.vue
+++ vue-connect-components/src/components/UserDetail.vue
@@ -2,13 +2,13 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>My name is {{name}}</p>
+        <p>My name is {{gogopowerrangers}}</p>
     </div>
 </template>
 
 <script>
 export default {
-  props: ["name"]
+  props: ["gogopowerrangers"]
 };
 </script>
 
