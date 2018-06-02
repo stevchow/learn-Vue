43e9c55a0a2318829ac01344efc3a285c3f9c4e8
stevchow <stephen_chow97@yahoo.co.id>
2018-06-02 20:41:19 +0700

    this method will render again and again without caching the component that already render out, so it bad for perfomance, because we need to http request the same stuff again and again, inefficient

--- vue-advancedComponent/src/components/author.vue
+++ vue-advancedComponent/src/components/author.vue
@@ -2,13 +2,23 @@
   <div>
     <h1>The Author</h1>
     <p>Welcome!</p>
+    <p>Medals count: {{counter}} </p>
+    <button @click="counter++">increase medals</button>
   </div>
 </template>
 
 <style>
-
 </style>
 
 <script>
-  
+export default {
+  data: function() {
+    return {
+      counter: 0
+    };
+  },
+  destroyed() {
+    console.log("I am destroyed");
+  }
+};
 </script>
