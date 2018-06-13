a29e64cb7d4e299954e4d7c4272de7005e3de325
stevchow <stephen_chow97@yahoo.co.id>
2018-06-13 18:08:40 +0700

    trying to use component from quoteGen on this project and succeed. Cool

--- vue-connect-components/src/App.vue
+++ vue-connect-components/src/App.vue
@@ -3,24 +3,27 @@
         <div class="row">
             <div class="col-xs-12">
                 <app-user></app-user>
+                <quote-gen></quote-gen>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import User from './components/User.vue';
+import User from "./components/User.vue";
+import QuoteGen from "./components/QuoteGen.vue";
 
-    export default {
-        components: {
-            appUser: User
-        }
-    }
+export default {
+  components: {
+    appUser: User,
+    QuoteGen
+  }
+};
 </script>
 
 <style>
-    div.component {
-        border: 1px solid black;
-        padding: 30px;
-    }
+div.component {
+  border: 1px solid black;
+  padding: 30px;
+}
 </style>
