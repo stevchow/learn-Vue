83f4391d6d5a9fa495557246f6ea47ca59fb2afd
stevchow <stephen_chow97@yahoo.co.id>
2018-06-17 20:03:03 +0700

    submit event by @click.prevent='yourmethodhere tochange boolean of form' create boolean and method

--- handling-user-input/src/App.vue
+++ handling-user-input/src/App.vue
@@ -108,13 +108,14 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+                            @click.prevent="submitted">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -157,8 +158,14 @@ export default {
       },
       dataValue: false,
       selectedPriority: "high",
-      priorities: ["high", "medium", "low"]
+      priorities: ["high", "medium", "low"],
+      isSubmitted: false
     };
+  },
+  methods: {
+    submitted() {
+      this.isSubmitted = true;
+    }
   },
   components: {
     appSwitch: Switch
