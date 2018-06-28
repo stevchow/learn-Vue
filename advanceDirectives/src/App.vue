315d5b27f5578d64bc94366862c04c3e80f90715
stevchow <stephen_chow97@yahoo.co.id>
2018-06-28 21:48:34 +0700

    passing object as a value on binding.value in html v-directive local tag

--- advanceDirectives/src/App.vue
+++ advanceDirectives/src/App.vue
@@ -15,7 +15,7 @@
     <h1>Change the input with color name, and it will change the hightlight color</h1>
     <input type="string" v-model="colorHighlight">
     <h1 v-highlightInput="colorHighlight">CHANGE MY HIGHLIGHT {{colorHighlight}}</h1>
-    <h1 v-local-highlight:colorText.delayedx.blink="colorHighlight">local - CHANGE MY HIGHLIGHT {{colorHighlight}}</h1>
+    <h1 v-local-highlight:colorText.delayedx.blink="{mainColor:colorHighlight, secondColor: 'white', delay:100}">local - CHANGE MY HIGHLIGHT {{colorHighlight}}</h1>
     <h1 v-highlightInput:colorText.delayed="colorHighlight">CHANGE MY text color with argument pass through custom v-directive:arg="value" {{colorHighlight}} - with 1s delay time</h1>
   </div>
 </template>
@@ -36,11 +36,11 @@ export default {
       componentUpdated(el, binding, vnode) {
         let delayTime = 0;
         if (binding.modifiers["delayedx"]) {
-          delayTime = 3000;
+          delayTime = 2000;
         }
         if (binding.modifiers["blink"]) {
-          let mainColor = binding.value;
-          let secondColor = "red";
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
           let curColor = mainColor;
           setTimeout(() => {
             setInterval(() => {
@@ -52,14 +52,14 @@ export default {
               } else {
                 el.style.backgroundColor = curColor;
               }
-            }, 500);
+            }, binding.value.delay);
           }, delayTime);
         } else {
           setTimeout(() => {
             if (binding.arg == "colorText") {
-              el.style.color = binding.value;
+              el.style.color = binding.value.mainColor;
             } else {
-              el.style.backgroundColor = binding.value;
+              el.style.backgroundColor = binding.value.mainColor;
             }
           }, delayTime);
         }
